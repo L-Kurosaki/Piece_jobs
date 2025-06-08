67fa1cbc0b36ec907b3e70d9435359b93492b071
Y-Kuro <103878257+L-Kurosaki@users.noreply.github.com>
2025-06-08 12:26:02 +0200

    Updated _layout.tsx

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,7 +1,7 @@
 import { Tabs } from 'expo-router';
 import { StyleSheet, View } from 'react-native';
 import Colors from '../../constants/Colors';
-import { Home, Briefcase, Plus, MessageSquare, User } from 'lucide-react-native';
+import { Chrome as Home, Briefcase, Plus, MessageSquare, User } from 'lucide-react-native';
 import NotificationBadge from '../../components/ui/NotificationBadge';
 
 export default function TabLayout() {
